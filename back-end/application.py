1e99bf2029e9efe3ad3c9dd200cdee04381ca5cf
Lucas <lucas.ma.dev@gmail.com>
2022-01-15 21:11:05 -0700

    Added the create endpoint

--- back-end/application.py
+++ back-end/application.py
@@ -2,9 +2,32 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return 'Hello!'
+class InventoryItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name}: {self.description}"
+
+def errorMessageWithCode(status, code):
+    return {"status": status}, code
+
+@app.route("/create", methods=["POST"])
+def createInventoryItem():
+    name = request.json["name"]
+    description = request.json["description"]
+
+    if name is None or name == "":
+        return errorMessageWithCode(400, "There was no item name given.")
+    if description is None:
+        description = ""
+
+    item = InventoryItem(name=name, description=description)
+    db.session.add(item)
+    db.session.commit()
+
+    return {"id": item.id}
